0326e29d0782fdd61cc4a066837bc7e279ce2957
henteko <henteko07@gmail.com>
2014-03-25 02:54:50 +0900

    add jasmine

---
+++ spec/javascripts/HatenaHttpJsSpec.js
@@ -0,0 +1,44 @@
+describe("HatenaHttp class is", function() {
+  var QRCODE_API_ENDPOINT = "http://chart.apis.google.com/chart?";
+  var hatena = null;
+
+  beforeEach(function() {
+    hatena = new HatenaHttp();
+  });
+
+  afterEach(function() {
+    hatena = null;
+  });
+
+  it("success image", function() {
+    var text = '[http://example.com/hoge.img:image]';
+    var result = '<img src=http://example.com/hoge.img>';
+    hatena.parse(text, function(replacedText) {
+      expect(replacedText).toEqual(result);
+    });
+  });
+
+  it("success barcode", function() {
+    var text = '[http://example.com:barcode]';
+    var result = '<img src=' + QRCODE_API_ENDPOINT + 'chs=150x150&cht=qr&chl=http://example.com>';
+    hatena.parse(text, function(replacedText) {
+      expect(replacedText).toEqual(result);
+    });
+  });
+
+  it("success set title", function() {
+    var text = '[http://example.com:title=example]';
+    var result = '<a href=http://example.com>example</a>';
+    hatena.parse(text, function(replacedText) {
+      expect(replacedText).toEqual(result);
+    });
+  });
+
+  it("not set type to generate link", function() {
+    var text = '[http://example.com]';
+    var result = '<a href=http://example.com>http://example.com</a>';
+    hatena.parse(text, function(replacedtext) {
+      expect(replacedtext).toequal(result);
+    });
+  });
+});
